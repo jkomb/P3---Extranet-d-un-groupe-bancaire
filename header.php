61377c1695adb149044f40fd9f8ab58361214b97
Jonathan KOMBEM <j.kombem@gmail.com>
2021-08-30 16:29:50 +0200

    Ajout du fichier de style + code header + évolution structure page principale

---
+++ header.php
@@ -0,0 +1,32 @@
+<header>
+
+<div id="logo">
+    <img src="images/GBAF.png" alt="Logo GBAF" />
+    <div>
+      <h2>Votre partenaire dans le temps</h2>
+    </div>
+</div>
+
+
+<!--
+Inclure "Vous êtes connecté en tant que xxx" en haut à droite
+Inclure l'avatar en haut à droite à droite du pseudo
+-->
+<div id="navigation">
+    <div>
+      <button id="deconnexion">Déconnexion</button></br>
+    </div>
+    <div>
+      Vous êtes connecté en tant que <strong>Jonathan KOMBEM</strong>
+    </div>
+    <div>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Mon Compte</a></li>
+        </ul>
+      </nav>
+    </div>
+</div>
+
+</header>
--- main.php
+++ main.php
@@ -5,44 +5,40 @@
   <title>Groupement Banque Assurance Français</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="main.css" />
 </head>
 
-    <body>
-        <div id="bloc_page">
-            <header>
-                <div id="titre_principal">
-                    <div id="logo">
-                        <img src="images/zozor_logo.png" alt="Logo de Zozor" />
-                        <h1>Zozor</h1>
-                    </div>
-                    <h2>Carnets de voyage</h2>
-                </div>
-
-                <nav>
-                    <ul>
-                        <li><a href="#">Accueil</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">CV</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
+<?php include('header.php');?>
 
             <div id="banniere_image">
+              <!--
                 <div id="banniere_description">
-                    Retour sur mes vacances aux États-Unis...
-                    <a href="#" class="bouton_rouge">Voir l'article <img src="images/flecheblanchedroite.png" alt="" /></a>
+                    Ensemble pour aller plus loin
                 </div>
+              -->
             </div>
 
-            <section>
+            <section class="presentation">
                 <article>
-                    <h1><img src="images/ico_epingle.png" alt="Catégorie voyage" class="ico_categorie" />Je suis un grand voyageur</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
-                    <p>Vivamus sed libero nec mauris pulvinar facilisis ut non sem. Quisque mollis ullamcorper diam vel faucibus. Vestibulum sollicitudin facilisis feugiat. Nulla euismod sodales hendrerit. Donec quis orci arcu. Vivamus fermentum magna a erat ullamcorper dignissim pretium nunc aliquam. Aenean pulvinar condimentum enim a dignissim. Vivamus sit amet lectus at ante adipiscing adipiscing eget vitae felis. In at fringilla est. Cras id velit ut magna rutrum commodo. Etiam ut scelerisque purus. Duis risus elit, venenatis vel rutrum in, imperdiet in quam. Sed vestibulum, libero ut bibendum consectetur, eros ipsum ultrices nisl, in rutrum diam augue non tortor. Fusce nec massa et risus dapibus aliquam vitae nec diam.</p>
-                    <p>Phasellus ligula massa, congue ac vulputate non, dignissim at augue. Sed auctor fringilla quam quis porttitor. Praesent vitae dignissim magna. Pellentesque quis sem purus, vel elementum mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas consectetur euismod urna. In hac habitasse platea dictumst. Quisque tincidunt porttitor vestibulum. Ut iaculis, lacus at molestie lacinia, ipsum mi adipiscing ligula, vel mollis sem risus eu lectus. Nunc elit quam, rutrum ut dignissim sit amet, egestas at sem.</p>
+                    <img src="images/GBAF.png" alt="Logo GBAF" class="logo" />
+                    <h1>Votre partenaire dans le temps</h1></br>
+                    Le Groupement Banque Assurance Français (<strong>GBAF</strong>) est une fédération représentant
+                    les 6 grands groupes français :
+                    <ul class="default">
+                        <li>BNP Paribas</li>
+                        <li>BPCE</li>
+                        <li>Crédit Agricole</li>
+                        <li>Crédit Mutuel-CIC</li>
+                        <li>Société Générale</li>
+                        <li>La Banque Postale</br></li>
+                    </ul>
+                    Même s’il existe une forte concurrence entre ces entités, elles vont toutes
+                    travailler de la même façon pour gérer près de 80 millions de comptes sur
+                    territoire national. Le <strong>GBAF</strong> est le représentant de la profession bancaire et des assureurs sur
+                    tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur
+                    privilégié des pouvoirs publics.</br>
                 </article>
+                <!--
                 <aside>
                     <h1>À propos de l'auteur</h1>
                     <img src="images/bulle.png" alt="" id="fleche_bulle" />
@@ -51,6 +47,63 @@
                     <p>Bien maigre, n'est-ce pas ? C'est pourquoi, aujourd'hui, j'ai décidé d'écrire ma biographie afin que vous sachiez qui je suis réellement.</p>
                     <p><img src="images/facebook.png" alt="Facebook" /><img src="images/twitter.png" alt="Twitter" /><img src="images/vimeo.png" alt="Vimeo" /><img src="images/flickr.png" alt="Flickr" /><img src="images/rss.png" alt="RSS" /></p>
                 </aside>
+              -->
+            </section>
+
+            <section class="section_acteurs">
+
+              <article class="article_acteur">
+                <a href="presentation_acteur.php?acteur=CDE" class="logo acteur">
+                  <img src="images/CDE.png" alt="CDE" class ="logo_acteurs" />
+                </a>
+                <div>
+                  <h2>CDE</h2>
+                  <p>Description de l'acteur/partenaire.</p>
+                </div>
+                <div<
+                  <button id="lireplus">Lire la suite</button>
+                </div>
+              </article>
+
+              <article class="article_acteur">
+                <a href="presentation_acteur.php?acteur=CDE" class="logo acteur">
+                  <img src="images/CDE.png" alt="CDE" class ="logo_acteurs" />
+                </a>
+                <div>
+                  <h2>CDE</h2>
+                  <p>Description de l'acteur/partenaire.</p>
+                </div>
+                <div<
+                  <button id="lireplus">Lire la suite</button>
+                </div>
+              </article>
+
+              <article class="article_acteur">
+                <a href="presentation_acteur.php?acteur=CDE" class="logo acteur">
+                  <img src="images/CDE.png" alt="CDE" class ="logo_acteurs" />
+                </a>
+                <div>
+                  <h2>CDE</h2>
+                  <p>Description de l'acteur/partenaire.</p>
+                </div>
+                <div<
+                  <button id="lireplus">Lire la suite</button>
+                </div>
+              </article>
+
+              <article class="article_acteur">
+                <a href="presentation_acteur.php?acteur=CDE" class="logo acteur">
+                  <img src="images/CDE.png" alt="CDE" class ="logo_acteurs" />
+                </a>
+                <div>
+                  <h2>CDE</h2>
+                  <p>Description de l'acteur/partenaire.</p>
+                </div>
+                <div<
+                  <button id="lireplus">Lire la suite</button>
+                </div>
+              </article>  
+
             </section>
 
             <footer>
