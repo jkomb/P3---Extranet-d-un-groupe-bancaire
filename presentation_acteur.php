83f0e835aa532fec28f6ccb16feb47d1813d8ef3
Jonathan KOMBEM <j.kombem@gmail.com>
2021-09-28 12:34:14 +0200

    ajout fonction commentaire unique

--- presentation_acteur.php
+++ presentation_acteur.php
@@ -33,14 +33,15 @@ if ( isset($_GET['acteur']) )
     exit;
   }
 
-  $request = $bdd -> prepare('SELECT acteur, description FROM acteurs WHERE id_acteur=:id_acteur LIMIT 0,1');
-  $request -> execute( array( 'id_acteur' => $id_acteur_choisi ) );
-  $info = $request -> fetch();
+  $acteur_request = $bdd -> prepare('SELECT acteur, description FROM acteurs WHERE id_acteur=:id_acteur LIMIT 0,1');
+  $acteur_request -> execute( array( 'id_acteur' => $id_acteur_choisi ) );
+  $info_acteur = $acteur_request -> fetch();
 
-  if ( !empty($info) )
+  if ( !empty($info_acteur) )
   {
     $exists_actor= true;
-    $sql_request = <<<SQL
+
+    $posts_request = <<<SQL
     SELECT accounts.username username, posts.post commentaire, posts.date_add date_com, posts.id_post
       FROM posts
         INNER JOIN accounts
@@ -49,10 +50,31 @@ if ( isset($_GET['acteur']) )
               ORDER BY date_com
                 DESC LIMIT 0, 5
     SQL;
-    $table_posts = $bdd -> prepare( $sql_request );
+    $table_posts = $bdd -> prepare( $posts_request );
     $table_posts -> execute( array( 'id_acteur' => $id_acteur_choisi ) );
-  }
 
+    $unique_post_request = <<<SQL
+    SELECT id_user
+      FROM accounts
+        WHERE EXISTS (
+          SELECT TRUE
+            FROM posts
+              WHERE accounts.id_user = posts.id_user)
+                LIMIT 0, 1
+    SQL;
+
+    $verify_unique_post = $bdd -> query( $unique_post_request );
+    $info = $verify_unique_post -> fetch();
+
+    if ( !in_array( $_SESSION['id_user'], $info ) )
+    {
+      $affichage_commentaire = true;
+    }
+    else
+    {
+      $affichage_commentaire = false;
+    }
+  }
   else
   {
     $exists_actor = false;
@@ -75,21 +97,25 @@ if ( $exists_actor === true )
 
         <div class="titre_logo_presentation">
           <br>
-          <img src="images/<?php echo $info['acteur']; ?>.png" alt="Logo <?php echo $info['acteur']; ?>"/ class="logo"/>
+          <img src="images/<?php echo $info_acteur['acteur']; ?>.png" alt="Logo <?php echo $info_acteur['acteur']; ?>"/ class="logo"/>
           <br>
         </div>
 
-        <?php echo nl2br( $info['description'] ); ?>
+        <?php echo nl2br( $info_acteur['description'] ); ?>
 
       </article>
 
     </section>
 
+<?php
+  if ( $affichage_commentaire )
+  {
+?>
   <section class="saisie_com">
     <form  method="post" action="presentation_acteur.php?acteur=<?php echo $id_acteur_choisi; ?>">
 
       <div>
-        <label><strong>Ajouter un commentaire</strong></label>
+        <label><strong>Ajouter un commentaire </strong>(vous ne pouvez donner votre avis qu'une seule fois!)</label>
       </div>
       <br>
       <div>
@@ -102,7 +128,19 @@ if ( $exists_actor === true )
 
     </form>
   </section>
-
+<?php
+  }
+  else
+  {
+?>
+  <section class="saisie_com">
+      <div>
+        <label><strong>Vous avez déjà donné votre avis ici.</strong></label>
+      </div>
+  </section>
+<?php
+  }
+?>
   <section class="section_commentaires">
 <?php
   if ( $_SESSION['admin'] === 1 )
